8a6a535f930cdf7745fe83086a3957fc11546cf7
Ricardo Neto <ricardofukushimaneto@gmail.com>
2025-08-11 15:53:35 -0300

    loans

---
+++ src/test/java/ricardo/products/loans/LoansApplicationTests.java
@@ -0,0 +1,13 @@
+package ricardo.products.loans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
